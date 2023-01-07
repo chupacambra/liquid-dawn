ac16f01dd34583e93f820307080e826d9ab828f5
chupacambra <chupacambra>
2023-01-07 17:45:48 +0200

    Mostly added the slideshow.

---
+++ assets/slider.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 'auto',
+  freeMode: true,
+  mousewheel: {
+    releaseOnEdges: true,
+  },
+});
+
+var thumb = document.querySelectorAll('.thumbContainer');
+
+thumb.forEach(function (image, index) {
+  var delay = index * 90;
+  image.classList.add('fadeInSlide');
+  image.style.animationDelay = delay + 'ms';
+});
